72f9b8f85bcff3b402458cbcb17139c30f7e2f91
Abdulhadi <a-ahmasani@hotmail.com>
2022-07-18 13:17:30 +0300

    create react --

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Earth </h1>
+      <h4>wecome to my app :) </h4>
+      <p>@copyright</p>
+    </div>
+  );
+}
+
+export default App;
